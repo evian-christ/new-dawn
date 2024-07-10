66790ef77a7f2c38b6707e77a79fe28a5ae5e915
evian-christ <thelastorcas@gmail.com>
2024-07-10 11:07:18 +0100

    basic movement implemented

---
+++ Practice/BasicMovement/Player.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+const SPEED = 200
+
+func _ready():
+	pass
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * SPEED
+		position += velocity * delta
