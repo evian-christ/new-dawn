68b5484b5c26f4847e2018d6c1853a09edd3a0b2
evian-christ <thelastorcas@gmail.com>
2024-07-16 15:34:25 +0100

    script for button practice

--- Practice/BasicMovement/buttonPractice.gd
+++ Practice/BasicMovement/buttonPractice.gd
@@ -1,11 +1,34 @@
 extends Node2D
 
+# Array to hold the image textures
+var images = []
+var current_image_index = 0
 
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time
 func _ready():
-	pass # Replace with function body.
+	# Load images into the array
+	images.append(load("res://image1.png"))
+	images.append(load("res://image2.png"))
+	images.append(load("res://image3.png"))
+	# Add more images as needed
+	
+	# Set the initial image
+	if $ImageDisplay:
+		$ImageDisplay.texture = images[current_image_index]
+	else:
+		print("ImageDisplay node not found in the scene.")
 
+	# Connect the button press signal to a function
+	if $NextImageButton:
+		$NextImageButton.pressed.connect(self, "_on_NextImageButton_pressed")
+	else:
+		print("NextImageButton node not found in the scene.")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# Function to handle button press
+func _on_NextImageButton_pressed():
+	# Change to the next image
+	current_image_index = (current_image_index + 1) % images.size()
+	if $ImageDisplay:
+		$ImageDisplay.texture = images[current_image_index]
+	else:
+		print("ImageDisplay node not found in the scene.")
